daa05a3a86b67afb569c1c012e74cd3230039f71
The Tech Academy <instructor@learncodinganywhere.com>
2019-11-25 14:55:31 -0800

    user thumb and posted img

--- src/app_component/stockTwits.component.jsx
+++ src/app_component/stockTwits.component.jsx
@@ -14,13 +14,26 @@ class StockTwits extends React.Component {
 
                 {
                     this.props.twitt.messages.map((msg, index) => {
+
+                        let postedImg='';
+                        let userAvatar= '';
+                        if(msg.entities.chart){
+                            postedImg = msg.entities.chart.url;
+                            
+                        }
+                        if(msg.user.avatar_url ){
+                           userAvatar = msg.user.avatar_url;
+                            
+                        }
                         return (
                             <div className="cards bg-light  text-dark mb-2 border border-info rounded text-left" key={index.toString()}>
                                 <div className="card-body py-0 pb-1 clearfix">
                                     <p className="card-text">
+                                        <object id='userImg' data= {userAvatar} type="image/jpg"><div></div></object> <br />
+                                        <span className='rounded mt-0 px-1 py-1 mr-2 userName'> {msg.user.name} </span>
+                                        <span className='rounded  mt-0 px-1 py-1 mr-2 twitDate'> {moment(msg.created_at).format('MMM Do YY, h:mm:ss a')} </span><br />
                                         <span className='twitBody'><span className="font-weight-bold"> </span> {msg.body}</span><br />
-                                        <span className='rounded px-1 py-1 mr-2 userName'> {msg.user.name}  </span>
-                                        <span className='rounded px-1 py-1 mr-2 twitDate'> {moment(msg.created_at).format('MMM Do YY, h:mm:ss a')} </span>
+                                        <object id='imgInPost' data= {postedImg} type="image/jpg"><div></div></object>
                                     </p>
                                 </div>
                             </div>
